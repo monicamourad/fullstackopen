3fd642ec9edf1d2883a00fc670f552ec59c9d5ab
Monica Mourad <monicamourad18@hotmail.com>
2025-01-15 21:04:39 +0100

    Submit Exercise 1.10

--- Part1/unicafe/src/Statistics.jsx
+++ Part1/unicafe/src/Statistics.jsx
@@ -1,22 +1,23 @@
 import Text from "./Text";
+import StatisticsLine from "./StatisticsLine";
 
 const Statistics = ({ good, neutral, bad, all, avg, positive }) => {
   return (
-    <>
+    <div>
       <h2>statistics</h2>
       {all === 0 ? (
         <Text text="No feedback given" />
       ) : (
         <>
-          <Text text={`good ${good}`} />
-          <Text text={`neutral ${neutral}`} />
-          <Text text={`bad ${bad}`} />
-          <Text text={`all ${all}`} />
-          <Text text={`average ${avg || 0}`} />
-          <Text text={`positive ${positive || 0} %`} />
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={avg} />
+          <StatisticsLine text="positive" value={positive} />
         </>
       )}
-    </>
+    </div>
   );
 };
 export default Statistics;
---
+++ Part1/unicafe/src/StatisticsLine.jsx
@@ -0,0 +1,4 @@
+import Text from "./Text";
+
+const StatisticsLine = ({ text, value }) => <Text text={`${text} ${value}`} />;
+export default StatisticsLine;
