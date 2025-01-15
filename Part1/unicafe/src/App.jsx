8cb5f95dcc30eab94533fa422a3434ca581bfc5c
Monica Mourad <monicamourad18@hotmail.com>
2025-01-15 20:05:11 +0100

    Submit Exercise 1.9

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <h2>give feedback</h2>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
--- Part1/unicafe/src/Statistics.jsx
+++ Part1/unicafe/src/Statistics.jsx
@@ -1,14 +1,22 @@
 import Text from "./Text";
 
-const Statistics = ({ good, neutral, bad, all, avg, positive }) => (
-  <>
-    <h2>statistics</h2>
-    <Text text={`good ${good}`} />
-    <Text text={`neutral ${neutral}`} />
-    <Text text={`bad ${bad}`} />
-    <Text text={`all ${all}`} />
-    <Text text={`average ${avg || 0}`} />
-    <Text text={`positive ${positive || 0} %`} />
-  </>
-);
+const Statistics = ({ good, neutral, bad, all, avg, positive }) => {
+  return (
+    <>
+      <h2>statistics</h2>
+      {all === 0 ? (
+        <Text text="No feedback given" />
+      ) : (
+        <>
+          <Text text={`good ${good}`} />
+          <Text text={`neutral ${neutral}`} />
+          <Text text={`bad ${bad}`} />
+          <Text text={`all ${all}`} />
+          <Text text={`average ${avg || 0}`} />
+          <Text text={`positive ${positive || 0} %`} />
+        </>
+      )}
+    </>
+  );
+};
 export default Statistics;
