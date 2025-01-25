d57aa0fd94fa0f9cd13146e5ebea5169d56be2f1
Monica Mourad <monicamourad18@hotmail.com>
2025-01-25 22:06:06 +0100

    Submit Exercise 2.9

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "040-1234567" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-1234567", key: "Arto Hellas" },
+  ]);
+  const [searchValue, setSearchValue] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const handleSearchValueChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -20,7 +27,9 @@ const App = () => {
     if (doesNameExists) {
       window.alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName, phone: newNumber }));
+      setPersons(
+        persons.concat({ name: newName, phone: newNumber, key: newName })
+      );
       setNewName("");
       setNewNumber("");
     }
@@ -28,22 +37,43 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with{" "}
+        <input
+          type="text"
+          value={searchValue}
+          onChange={handleSearchValueChange}
+        />
+      </div>
+      <h2>Add a New</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input type="text" value={newName} onChange={handleNameChange} />
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input type="number" value={newNumber} onChange={handleNumberChange} />
+          number:{" "}
+          <input
+            type="number"
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name} {person.phone}</p>
-      ))}
+      {persons.map(
+        (person) =>
+          (!searchValue ||
+            person.name.toLowerCase().includes(searchValue.toLowerCase())) && (
+            <p key={person.name}>
+              {person.name} {person.phone}
+            </p>
+          )
+      )}
     </div>
   );
 };
